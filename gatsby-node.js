3581f76a9bc26087b0ff6524454b929e8a325e16
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2020-03-08 15:44:41 -0700

    Adding case studies

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,3 +19,40 @@ exports.createSchemaCustomization = ({ actions }) => {
   `
   createTypes(typeDefs)
 }
+const path = require('path')
+
+exports.createPages = ({graphql, boundActionCreators}) => {
+  const {createPage} = boundActionCreators
+  return new Promise((resolve, reject) => {
+    const storeTemplate = path.resolve('src/templates/caseStudy.js')
+    resolve(
+      graphql(`
+        {
+          allContentfulCaseStudy (limit:100) {
+            edges {
+              node {
+                id
+                name
+                slug
+              }
+            }
+          }
+        }
+      `).then((result) => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+        result.data.allContentfulCaseStudy.edges.forEach((edge) => {
+          createPage ({
+            path: edge.node.slug,
+            component: caseStudyTemplate,
+            context: {
+              slug: edge.node.slug
+            }
+          })
+        })
+        return
+      })
+    )
+  })
+}
--- src/components/gallery-image/GalleryImage.js
+++ src/components/gallery-image/GalleryImage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import Img from 'gatsby-image';
 // import Grid from '@material-ui/core/Grid';
- import Fade from '@material-ui/core/Fade'
+import Fade from '@material-ui/core/Fade'
 
 import styles from './style';
 
