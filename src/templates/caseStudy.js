df9c9aab5890aac30d9665465693c2044e649c19
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2020-03-09 02:01:53 -0700

    Styling case studies.

--- src/templates/caseStudy.js
+++ src/templates/caseStudy.js
@@ -3,24 +3,27 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Img from "gatsby-image";
 import Grid from '@material-ui/core/Grid';
+import styles from './style';
+
+type Props = {
+  classes: Object,
+};
 
 export default ({ data }) => {
 const page = data.contentfulCaseStudy;
 return (
     <Layout>
-        <div>
-            <h1>{page.title}</h1>
-            <h2>{page.description.description}</h2>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={4}>
-                    <div>
+    <div className={classes.container}>
+        <h1>{page.title}</h1>
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+            <p>{page.description.description}</p>
                         {page.gallery.map(({ fluid }, index) => (
                             <Img key={index} fluid={fluid} />
                         ))}
-                    </div>
                 </Grid>
-            </Grid>          
-        </div>
+            </Grid>    
+    </div>     
     </Layout>
 );
 };
---
+++ src/templates/style.js
@@ -0,0 +1,9 @@
+const styles = ({ typography }) => ({
+    container: {
+        padding: `${typography.pxToRem(48)} ${typography.pxToRem(4)} `,
+        border: '2px',
+        borderColor: '#707070',
+    }
+});
+
+export default styles;
