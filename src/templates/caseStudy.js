c73fc2a90796c1d2c4050ec4e876277faf1479bb
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2020-03-09 02:41:41 -0700

    Case Study layout

--- src/templates/caseStudy.js
+++ src/templates/caseStudy.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../components/layout';
+import { withStyles } from '@material-ui/styles';
+import {Layout, SEO} from '../components';
 import Img from "gatsby-image";
-import Grid from '@material-ui/core/Grid';
+import {Grid, Container} from '@material-ui/core';
 import styles from './style';
+
 
 type Props = {
   classes: Object,
 };
 
-export default ({ data }) => {
+const CaseStudy = ({ data }, { classes }: Props) => {
 const page = data.contentfulCaseStudy;
 return (
     <Layout>
-    <div className={classes.container}>
+    <SEO title={page.title}/>
+    <Container maxWidth='lg'>
         <h1>{page.title}</h1>
-        <Grid container spacing={2}>
-            <Grid item xs={12} md={4}>
+        <Grid container spacing={2} direction="row"
+  justify="center"
+  alignItems="flex-start">
+            
+            <Grid item xs={12} md={3}>
             <p>{page.description.description}</p>
-                        {page.gallery.map(({ fluid }, index) => (
-                            <Img key={index} fluid={fluid} />
+                        {page.gallery.map(({ fluid }) => (
+                            <Img fluid={fluid} />
                         ))}
-                </Grid>
-            </Grid>    
-    </div>     
+                        </Grid>
+            </Grid>   
+            </Container>     
     </Layout>
 );
 };
@@ -43,3 +49,4 @@ export const query = graphql`
     }
 
 `
+export default withStyles(styles)(CaseStudy)
--- src/templates/style.js
+++ src/templates/style.js
@@ -2,8 +2,12 @@ const styles = ({ typography }) => ({
     container: {
         padding: `${typography.pxToRem(48)} ${typography.pxToRem(4)} `,
         border: '2px',
-        borderColor: '#707070',
-    }
+        borderColor: '#707070'
+    },
+    gridList: {
+    width: 500,
+    height: 450,
+  }
 });
 
 export default styles;
