afc3cde15a2566b7e5501aedd8e0040933a0ea13
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2020-03-10 18:08:39 -0700

    Built mosaic gallery into the case studies

--- src/templates/caseStudy.js
+++ src/templates/caseStudy.js
@@ -3,8 +3,9 @@ import { graphql } from 'gatsby';
 import { withStyles } from '@material-ui/styles';
 import {Layout, SEO} from '../components';
 import Img from "gatsby-image";
-import {Grid, Container} from '@material-ui/core';
+import {Grid, Container, GridList, GridListTile} from '@material-ui/core';
 import styles from './style';
+import StackGrid from "react-stack-grid"
 
 
 type Props = {
@@ -17,18 +18,27 @@ return (
     <Layout>
     <SEO title={page.title}/>
     <Container maxWidth='lg'>
-        <h1>{page.title}</h1>
+        
         <Grid container spacing={2} direction="row"
-  justify="center"
-  alignItems="flex-start">
+            justify="space-around"
+            alignItems="flex-Start">
             
-            <Grid item xs={12} md={3}>
+            <Grid item xs={12} md={6}>
+                        
+            </Grid>
+            
+            </Grid>  
+            <StackGrid columnWidth={'33%'} gutterWidth={20} gutterHeight={20}>
+                <div>
+            <h1>{page.title}</h1>
             <p>{page.description.description}</p>
+            </div>
                         {page.gallery.map(({ fluid }) => (
+                        <div>
                             <Img fluid={fluid} />
+                            </div>
                         ))}
-                        </Grid>
-            </Grid>   
+            </StackGrid>
             </Container>     
     </Layout>
 );
