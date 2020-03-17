35788efdc63aa1b035007a65781bb70428386169
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2020-03-17 16:10:40 -0700

    Adding the contact form

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
-
+import { useForm, Controller} from "react-hook-form";
+import { Box, Input, Button, TextField } from '@material-ui/core'
+import * as yup from 'yup'
 import styles from './style';
+
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().required(),
+  message: yup.string().required(),
+
+})
 
 type Props = {
   classes: Object,
 };
 
-const Footer = ({ classes }: Props) => (
-  <footer className={classes.container}>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href='https://www.gatsbyjs.org'  >Gatsby</a>
+
+function Footer({ classes }: Props) {  
+  const { control, register, handleSubmit, errors } = useForm({ validationSchema: schema })
+  const onSubmit = data => console.log(data);
+  console.log(errors)
+  return(
+  <div>
+  <footer className={classes.topContainer}>
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <Box>
+      <Controller as={TextField} className={classes.inputField} placeholder="First name" name="firstName" ref={register({required: true, maxLength: 80})} control={control} defaultValue=""/>
+      {errors.firstName && 'First name is required'}
+      <Controller as={Input} className={classes.inputField} type ='text'placeholder="Last name" name="lastName" ref={register({required: true, maxLength: 100})} control={control} defaultValue=""/>
+      {errors.lastName && 'Last name is required'}
+      <Controller as={Input} className={classes.inputField}  type='text' placeholder="Email" name="email" ref={register({required: true, pattern: /^\S+@\S+$/i})} control={control} defaultValue=""/>
+      {errors.email && 'Please submit a valid email address'}
+      <Controller as={Input} className={classes.inputField}  type="text" placeholder="Message" name="message" ref={register({required: true, maxLength: 2000})} control={control} defaultValue=""/>
+      </Box>
+      <Button type='submit'>Submit</Button>
+    </form>
+    </footer>
+  <footer className={classes.bottomContainer}>
+    © {new Date().getFullYear()}, Built by Color Mill Design with
+    {' '}
+    <a href='https://www.gatsbyjs.org' className={classes.linkOut} >Gatsby</a>
   </footer>
+  </div>
 );
+}
 
 export default withStyles(styles)(Footer);
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,5 +1,5 @@
 const styles = ({ app, typography }) => ({
-  container: {
+  bottomContainer: {
     display: 'flex',
     justifyContent: 'left',
     alignItems: 'center',
@@ -11,8 +11,31 @@ const styles = ({ app, typography }) => ({
     fontWeight: 300,
     fontSize: typography.pxToRem(15),
     color: 'white',
-    linkColor: 'white'
+    linkColor: 'white', 
+      },
+  topContainer:{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: `${typography.pxToRem(2)}`,
+    maxWidth: 'app.maxWidth',
+    width: '33.3%',
+    background: '#E8E8E8',
+    fontWeight: 500,
+    fontSize: typography.pxToRem(15),
+    color: 'primary',
   },
+  inputField:{
+    background: 'white',
+    width: '100%',
+    margin: `${typography.pxToRem(5)}`,
+    color: 'primary',
+  },
+  linkOut: {
+    color: 'white',
+    linkColor: 'white',
+    padding: '.3rem'
+}
 });
 
 export default styles;
