2ad32a815b23f109b2711d0b052455687056984e
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2020-07-06 08:36:52 -0700

    Expires headers plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     //`gatsby-plugin-sitemap`,
+    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -52,5 +53,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-headers`,
   ],
 }
